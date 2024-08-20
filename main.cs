6c1d5333aef1d05b6e5c7d9992bd15faf7a7d213
Alexander Petrov <101967173+AlexXxShurik@users.noreply.github.com>
2024-08-20 09:16:24 +0500

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Crickets : MonoBehaviour
+{
+    void Update()
+    {
+        Debug.Log("crickets");
+    }
+}
+
+
+public class Destroy : MonoBehaviour
+{
+    void Start()
+    {
+        Destroy(gameObject);
+    }
+}
+
+
+public class CreatePrimitives : MonoBehaviour
+{
+    void Start()
+    {
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0, 0, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(-0.5f, 1, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0.5f, 1, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0.5f, 2, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(-0.5f, 2, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0, 3, 0);
+    }
+}
